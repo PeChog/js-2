b15853b76125270b50b87826de21d3aee2d789bb
Pierre-Etienne <pierre-etienne@MacBook-Pro-de-Pierre-Etienne.local>
2023-01-10 16:51:55 +0100

    scroll ok

--- 17_scroll.js
+++ 17_scroll.js
@@ -1,1 +1,17 @@
+let lastScroll = 0;
 
+window.addEventListener("scroll", () => {
+  const date = new Date();
+  date.toString();
+  if (window.scrollY > lastScroll) {
+    let p = document.createElement("p");
+    p.textContent = date;
+    document.getElementsByTagName("body")[0].appendChild(p);
+    console.log("element créé");
+  }
+  lastScroll = window.scrollY;
+});
+
+console.log(lastScroll);
+
+console.log(document.getElementsByTagName("p"));
---
+++ 19_keyBinding.js
@@ -0,0 +1,27 @@
+// const square = document.getElementById("square");
+
+// square.style.top = "0px";
+// square.style.left = "0px";
+
+// document.addEventListener("keydown", (event) => {
+//   console.log(event.key);
+//   if ((event.key = "ArrowUp")) {
+//     square.style.top = `${square.style.top + 10}px`;
+//   }
+
+//   if ((event.key = "ArrowRight")) {
+//     square.style.right = square.style.left + 10;
+//   }
+//   if ((event.key = "ArrowLeft")) {
+//     square.style.right = `${square.style.left - 10}px`;
+//   }
+//   if ((event.key = "ArrowRight")) {
+//     square.style.right = `${square.style.left - 10}px`;
+//   }
+
+//   let right = (event.key = "ArrowRight");
+//   let down = (event.key = "ArrowDown");
+//   let left = (event.key = "ArrowLeft");
+// });
+
+// console.log(square);
