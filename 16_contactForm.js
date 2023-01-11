891f1a5f24e0e763a0f67b2ea36dd7744466616d
Pierre-Etienne <pierre-etienne@MacBook-Pro-de-Pierre-Etienne.local>
2023-01-11 15:24:32 +0100

    changes 16

--- 16_contactForm.js
+++ 16_contactForm.js
@@ -1,14 +1,11 @@
 const createTag = () => {
   const age = document.getElementsByName("age")[0].value;
-  console.log(age);
   const firstName = document.getElementsByName("name")[0].value;
-  console.log(firstName);
   const date = document.getElementsByName("birthdate")[0].value;
-  console.log(date);
-
   const tag = document.createElement("p");
-  tag.textContent = `name: ${firstName}, age: ${age}, birthdate: ${date} `;
+  tag.textContent = `name: ${firstName}, age: ${age}, birthdate: ${date}`;
   tag.setAttribute("id", "result");
+
   return document.getElementsByTagName("body")[0].appendChild(tag);
 };
 createTag();
--- 20_dynamicFont.js
+++ 20_dynamicFont.js
@@ -2,6 +2,8 @@ let value = document.getElementById("fontsize");
 
 let toChange = document.getElementsByClassName("controlled_fontsize");
 toChange = [...toChange];
+
 const changeFontSize = () =>
   toChange.map((e) => (e.style.fontSize = `${value.value}px`));
+
 value.addEventListener("input", changeFontSize);
